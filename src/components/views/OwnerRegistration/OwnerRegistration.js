0d8b911828c414c854e97b9c9989d33c4294e87c
bkchu <brandon.k.chung@gmail.com>
2018-04-30 19:44:13 -0500

    added prop types package, added owner registration form and classnames

--- src/components/views/OwnerRegistration/OwnerRegistration.js
+++ src/components/views/OwnerRegistration/OwnerRegistration.js
@@ -1,14 +1,114 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {createOwner} from '../../../redux/ducks/users';
 import './OwnerRegistration.css';
 
-const OwnerRegistration = () => (
-  <div>
-    This is the owner registration page.
-    <div>
-      Already have an account? <Link to="/login/owner/returning">Sign In.</Link>
-    </div>
-  </div>
-);
+class OwnerRegistration extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      companyName: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    };
 
-export default OwnerRegistration;
+    this.onChangeHandler = this.onChangeHandler.bind(this);
+    this.onSubmitHandler = this.onSubmitHandler.bind(this);
+  }
+
+  onChangeHandler(e) {
+    return this.setState({[e.target.name]: e.target.value});
+  }
+
+  onSubmitHandler(e) {
+    e.preventDefault();
+    this.props.register({...this.state});
+  }
+
+  render() {
+    const {
+      firstName, lastName, companyName, email, password, confirmPassword,
+    } = this.state;
+    return (
+      <div className="OwnerRegistration">
+        <div className="OwnerRegistration__form">
+          <form onSubmit={this.onSubmitHandler}>
+            <input
+              className="OwnerRegistration__input OwnerRegistration__input--firstname"
+              onChange={this.onChangeHandler}
+              required
+              placeholder="firstName"
+              name="firstName"
+              maxLength="35"
+              value={firstName}
+              type="text"
+            />
+            <input
+              className="OwnerRegistration__input OwnerRegistration__input--lastname"
+              onChange={this.onChangeHandler}
+              required
+              placeholder="lastName"
+              name="lastName"
+              maxLength="35"
+              value={lastName}
+              type="text"
+            />
+            <input
+              className="OwnerRegistration__input OwnerRegistration__input--companyname"
+              onChange={this.onChangeHandler}
+              placeholder="companyName"
+              name="companyName"
+              value={companyName}
+              type="text"
+            />
+            <input
+              className="OwnerRegistration__input OwnerRegistration__input--email"
+              onChange={this.onChangeHandler}
+              required
+              placeholder="email"
+              name="email"
+              value={email}
+              type="email"
+            />
+            <input
+              className="OwnerRegistration__input OwnerRegistration__input--password"
+              onChange={this.onChangeHandler}
+              required
+              placeholder="password"
+              name="password"
+              value={password}
+              type="password"
+            />
+            <input
+              className="OwnerRegistration__input OwnerRegistration__input--confirmpassword"
+              onChange={this.onChangeHandler}
+              required
+              placeholder="confirmPassword"
+              name="confirmPassword"
+              value={confirmPassword}
+              type="password"
+            />
+            <button>Register</button>
+          </form>
+          <div className="OwnerRegistration__sign-in">
+            Already have an account?{' '}
+            <Link className="Link__none" to="/login/owner/returning">
+              Sign In.
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+OwnerRegistration.propTypes = {
+  register: PropTypes.func.isRequired,
+};
+
+export default connect(null, {createOwner})(OwnerRegistration);
