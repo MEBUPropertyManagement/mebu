89c4f07c8755260df980300f7d1c7aa94c09dcce
Michael Kovich <kovich@michaelkovich.com>
2018-05-02 09:04:58 -0500

    Added addResident endpoint and import.

--- server/index.js
+++ server/index.js
@@ -13,8 +13,31 @@ const {
   ownerRegistration,
   residentLogin,
   ownerLogin,
+  addResident,
   logout,
 } = require(`${__dirname}/controllers/authenticationControllers`);
+
+const {
+  addProperties,
+  getProperty,
+  getProperties,
+} = require(`${__dirname}/controllers/propertiesControllers`);
+
+const {addUnit, getUnitById} = require(`${__dirname}/controllers/unitsControllers`);
+
+const {getResidents} = require(`${__dirname}/controllers/residentsControllers`);
+
+const {
+  workOrderByPropertyId,
+  workOrderByResidentId,
+  addWorkOrder,
+} = require(`${__dirname}/controllers/workorderControllers`);
+
+const {
+  addBill,
+  getBillingHistory,
+  getAllUnpaidBills,
+} = require(`${__dirname}/controllers/billsControllers`);
 
 require('dotenv').config();
 
@@ -45,7 +68,25 @@ app.post('/users/resident-registration', residentRegistration);
 app.post('/users/resident-login', residentLogin);
 app.post('/users/owner-registration', ownerRegistration);
 app.post('/users/owner-login', ownerLogin);
+app.post('/users/add', addResident);
 app.post('/users/logout', logout);
+
+app.post('/unit/add', addUnit);
+app.get('/units/getById/:id', getUnitById);
+
+app.post('/properties/add', addProperties);
+app.get('/properties/getProperty/:id', getProperty);
+app.get('/properties/getProperties', getProperties);
+
+app.get('/residents/getById/:id', getResidents);
+
+app.get('/workorder/getByPropertyId/:id', workOrderByPropertyId);
+app.get('/workorder/getByResidentId/:id', workOrderByResidentId);
+app.post('/workorder/addWorkorder', addWorkOrder);
+
+app.post('/bills/add', addBill);
+app.get('/bills/getBillingHistory', getBillingHistory);
+app.get('/bills/getAllUnpaidBills', getAllUnpaidBills);
 
 app.listen(port, () => {
   console.log(`Listening on Port: ${port}`);
