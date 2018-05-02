6a14d15f131f9e12bc61b38dd2efbe7011e7623c
bkchu <brandon.k.chung@gmail.com>
2018-05-02 12:01:41 -0500

    added residentReducer and can load residents for given property id

--- src/components/views/Dashboard/Residents/Residents.js
+++ src/components/views/Dashboard/Residents/Residents.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {getResidents} from '../../../../redux/ducks/residentReducer';
 
-const Residents = () => (
-  <div>
-    <p>This is the residents component.</p>
-  </div>
-);
+class Residents extends Component {
+  componentDidMount() {
+    this.props.getResidents(this.props.match.params.id);
+  }
 
-export default Residents;
+  render() {
+    const {residents, loading} = this.props;
+    let residentDisplay = <p>Loading...</p>;
+    if (residents && residents[0] && !loading) {
+      residentDisplay = residents.map(resident => (
+        <div key={resident.residentid}>
+          <p>{`${resident.firstname} ${resident.lastname} ${resident.email}`}</p>
+        </div>
+      ));
+    }
+
+    return <div className="Residents">{residentDisplay}</div>;
+  }
+}
+
+const mapStateToProps = state => ({
+  ...state.residentReducer,
+});
+
+export default connect(mapStateToProps, {getResidents})(Residents);
