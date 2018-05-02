7849f97d2486f96b255068ee33467ad854de3692
Michael Kovich <kovich@michaelkovich.com>
2018-05-02 11:02:28 -0500

    Even more testing.

--- cypress/integration/ownerLogin.js
+++ cypress/integration/ownerLogin.js
@@ -1,29 +1,29 @@
 describe('Header Text', () => {
   it('Should have a clickable owner login button', () => {
     cy
-      .visit('http://localhost:3001/')
+      .visit('http://localhost:3000/')
       .get('.Landing_link--owner')
       .click();
   });
 
   it('Should have an input for an email address', () => {
-    cy.get('input').should('have.class', XXX);
+    cy.get('input').should('have.class', 'OwnerLogin__input--email');
   });
 
   it('Should have an input for a password', () => {
-    cy.get('input').should('have.class', XXX);
+    cy.get('input').should('have.class', 'OwnerLogin__input--password');
   });
 
   it('Should accept input into the email address field', () => {
-    cy.get(XXX).type('aa@aa.com');
+    cy.get('OwnerLogin__input--email').type('aa@aa.com');
   });
 
   it('Should accept input into the password field', () => {
-    cy.get(XXX).type('tailopez');
+    cy.get('OwnerLogin__input--password').type('tailopez');
   });
 
   it('Should submit the form when the login button is clicked', () => {
-    cy.get(XXX).click();
+    cy.get('OwnerLogin__submit').click();
   });
 
   it('Should redirect to a page with a property display class', () => {
