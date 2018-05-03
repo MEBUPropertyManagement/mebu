4b2d5e416906d6e22657251b4e30b6d8ab7f5978
uukomadu <ugoukomadu@gmail.com>
2018-05-03 10:27:21 -0500

    maintenance update

--- src/components/views/Dashboard/Maintenance/Maintenance.js
+++ src/components/views/Dashboard/Maintenance/Maintenance.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {getPropertyById} from '../../../../redux/ducks/propertyReducer';
+import {connect} from 'react-redux';
 
-const Maintenance = () => (
-  <div>
-    <p>This is the maintenance component.</p>
-  </div>
-);
+class Maintenance extends Component {
+  componentDidMount() {
+    console.log(this.props);
+    this.props.getPropertyById(this.props.match.params.id);
+  }
 
-export default Maintenance;
+  render() {
+    const maintenance = <p>...loading</p>;
+    console.log(this.props.selectedProperty);
+    return (
+      <div>
+        <p>This is the maintenance component.</p>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  ...state.propertyReducer,
+});
+
+export default connect(mapStateToProps, {getPropertyById})(Maintenance);
