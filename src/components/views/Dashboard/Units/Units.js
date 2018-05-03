8bcc7835768ff561089fd300c44ca806211723c9
bkchu <brandon.k.chung@gmail.com>
2018-05-03 10:25:20 -0500

    units view WIP

--- src/components/views/Dashboard/Units/Units.js
+++ src/components/views/Dashboard/Units/Units.js
@@ -1,6 +1,7 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import {connect} from 'react-redux';
 import {getPropertyById} from '../../../../redux/ducks/propertyReducer';
+import './Units.css';
 
 class Units extends Component {
   componentDidMount() {
@@ -9,22 +10,28 @@ class Units extends Component {
   }
 
   render() {
+    const {selectedProperty, loading} = this.props;
     let property = <p>...loading</p>;
-    if (this.props.selectedProperty && !this.props.loading) {
-      console.log(this.props.selectedProperty);
-      const prop = this.props.selectedProperty;
-      property = (
-        <div>
-          <h1>Name: {prop.name}</h1>
-          <p>Address: {prop.address}</p>
-          <p>Units: {prop.units}</p>
-          <p>Value: {prop.value}</p>
-          <p>Expenses: {prop.expenses}</p>
-        </div>
-      );
+    if (selectedProperty && !loading) {
+      console.log(selectedProperty);
+      const prop = selectedProperty;
+      property =
+        selectedProperty.occupiedUnits &&
+        selectedProperty.occupiedUnits.map(unit => (
+          <Fragment>
+            <p>bath: {unit.bath}</p>
+            <p>bed: {unit.bed}</p>
+            <p>occupied: {unit.occupied}</p>
+            <p>propertyid: {unit.propertyid}</p>
+            <p>rent: {unit.rent}</p>
+            <p>roomnum: {unit.roomnum}</p>
+            <p>size: {unit.size}</p>
+            <p>unitid: {unit.unitid}</p>
+          </Fragment>
+        ));
     }
 
-    return <div>{property}</div>;
+    return <div className="Units container">{property}</div>;
   }
 }
 
