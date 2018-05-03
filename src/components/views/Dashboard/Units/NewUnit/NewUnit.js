f5c35f407e67083c95db1a0458db02d77c70bec1
bkchu <brandon.k.chung@gmail.com>
2018-05-03 13:57:33 -0500

    finished updating NewUnits

--- src/components/views/Dashboard/Units/NewUnit/NewUnit.js
+++ src/components/views/Dashboard/Units/NewUnit/NewUnit.js
@@ -25,26 +25,32 @@ class NewUnit extends Component {
   onEditHandler() {
     if (this.state.editing) {
       const {propertyid, unitid, size} = this.props.unit;
-      axios.post('/unit/update', {
-        ...this.state, propertyid, unitid, size,
-      }).then((response) => {
-        console.log('response.data: ', response.data);
-        const {
-          bath, bed, occupied, rent, roomnum,
-        } = response.data[0];
-        this.setState({
-          bath,
-          bed,
-          occupied,
-          rent,
-          roomnum,
+      axios
+        .post('/unit/update', {
+          ...this.state,
+          propertyid,
+          unitid,
+          size,
+        })
+        .then((response) => {
+          console.log('response.data: ', response.data);
+          const {
+            bath, bed, occupied, rent, roomnum,
+          } = response.data[0];
+          this.setState({
+            bath,
+            bed,
+            occupied,
+            rent,
+            roomnum,
+          });
         });
-      });
     }
     this.setState(prevState => ({editing: !prevState.editing}));
   }
 
   render() {
+    console.log(this.state);
     const {
       bath, bed, occupied, rent, roomnum, size,
     } = this.props.unit;
@@ -97,11 +103,11 @@ class NewUnit extends Component {
       </Fragment>
     ) : (
       <Fragment>
-        <p>{bath}</p>
-        <p>{bed}</p>
-        <p>{occupied ? 'Yes' : 'No'}</p>
-        <p>{rent}</p>
-        <p>{roomnum}</p>
+        <p>{this.state.bath}</p>
+        <p>{this.state.bed}</p>
+        <p>{this.state.occupied ? 'Yes' : 'No'}</p>
+        <p>{this.state.rent}</p>
+        <p>{this.state.roomnum}</p>
         <p>{size}</p>
       </Fragment>
     );
