16f325d935480debce6689e8f5549eb977bf77cc
Michael Kovich <kovich@michaelkovich.com>
2018-05-03 15:28:16 -0500

    Created resident dashboard components, added conditional rendering to dashboard.js, and added resident dashboard routes.

--- src/components/views/Dashboard/Dashboard.js
+++ src/components/views/Dashboard/Dashboard.js
@@ -1,41 +1,66 @@
 import React from 'react';
 import {Link, withRouter} from 'react-router-dom';
 
-const Dashboard = props => (
-  <div>
-    <Link className="Link__none" to={`/owner/dashboard/property/${props.match.params.id}`}>
-      <h1>Dashboard</h1>
-    </Link>
-    <nav>
-      <Link className="Link__none" to={`/owner/dashboard/property/${props.match.params.id}/units`}>
-        Units
+const Dashboard = (props) => {
+  const {path} = props.match;
+
+  return path.includes('/owner/') ? (
+    <div>
+      <Link className="Link__none" to={`/owner/dashboard/property/${props.match.params.id}`}>
+        <h1>Dashboard</h1>
       </Link>
-      <Link
-        className="Link__none"
-        to={`/owner/dashboard/property/${props.match.params.id}/residents`}
-      >
-        Residents
+      <nav>
+        <Link
+          className="Link__none"
+          to={`/owner/dashboard/property/${props.match.params.id}/units`}
+        >
+          Units
+        </Link>
+        <Link
+          className="Link__none"
+          to={`/owner/dashboard/property/${props.match.params.id}/residents`}
+        >
+          Residents
+        </Link>
+        <Link
+          className="Link__none"
+          to={`/owner/dashboard/property/${props.match.params.id}/metrics`}
+        >
+          Metrics
+        </Link>
+        <Link
+          className="Link__none"
+          to={`/owner/dashboard/property/${props.match.params.id}/maintenance`}
+        >
+          Maintenance
+        </Link>
+        <Link
+          className="Link__none"
+          to={`/owner/dashboard/property/${props.match.params.id}/settings`}
+        >
+          Settings
+        </Link>
+      </nav>
+      {props.children}
+    </div>
+  ) : (
+    <div>
+      <Link className="Link__none" to="/resident/dashboard/">
+        Contacts
       </Link>
-      <Link
-        className="Link__none"
-        to={`/owner/dashboard/property/${props.match.params.id}/metrics`}
-      >
-        Metrics
+      <Link className="Link__none" to="/resident/dashboard/billing/pay">
+        Pay Bills
       </Link>
-      <Link
-        className="Link__none"
-        to={`/owner/dashboard/property/${props.match.params.id}/maintenance`}
-      >
-        Maintenance
+      <Link className="Link__none" to="/resident/dashboard/billing/history">
+        Billing History
       </Link>
-      <Link
-        className="Link__none"
-        to={`/owner/dashboard/property/${props.match.params.id}/settings`}
-      >
-        Settings
+      <Link className="Link__none" to="/resident/dashboard/maintenance/create">
+        Maintenance Request
       </Link>
-    </nav>
-    {props.children}
-  </div>
-);
+      <Link className="Link__none" to="/resident/dashboard/maintenance/history">
+        Maintenance History
+      </Link>
+    </div>
+  );
+};
 export default withRouter(Dashboard);
