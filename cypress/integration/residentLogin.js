af54f7b17f2b0373176307136b9c4818e8a4b663
Michael Kovich <kovich@michaelkovich.com>
2018-05-03 10:23:22 -0500

    ResidentLogin E2E testing, class change to ResidentLogin

--- cypress/integration/residentLogin.js
+++ cypress/integration/residentLogin.js
@@ -1,37 +1,37 @@
 describe('Header Text', () => {
   it('Should have a clickable resident login button', () => {
     cy
-      .visit('http://localhost:3001/')
+      .visit('http://localhost:3000/')
       .get('.Landing_link--resident')
       .click();
   });
 
   it('Should have an input for an email address', () => {
-    cy.get('input').should('have.class', XXX);
+    cy.get('input').should('have.class', 'ResidentLogin__input--email');
   });
 
   it('Should have an input for a password', () => {
-    cy.get('input').should('have.class', XXX);
+    cy.get('input').should('have.class', 'ResidentLogin__input--password');
   });
 
   it('Should accept input into the email address field', () => {
-    cy.get(XXX).type('aa@aa.com');
+    cy.get('.ResidentLogin__input--email').type('christoph@chriswf.co');
   });
 
   it('Should have its value updated with the user input', () => {
-    cy.get(XXX).should('have.value', 'aa@aa.com');
+    cy.get('.ResidentLogin__input--email').should('have.value', 'christoph@chriswf.co');
   });
 
   it('Should accept input into the password field', () => {
-    cy.get(XXX).type('tailopez');
+    cy.get('.ResidentLogin__input--password').type('potatoes');
   });
 
   it('Should have its value updated with the user input', () => {
-    cy.get(XXX).should('have.value', 'tailopez');
+    cy.get('.ResidentLogin__input--password').should('have.value', 'potatoes');
   });
 
   it('Should submit the form when the login button is clicked', () => {
-    cy.get(XXX).click();
+    cy.get('.ResidentLogin__submit').click();
   });
 
   // it('Should redirect to a page with a display class', () => {
