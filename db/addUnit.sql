5cad8d785d990c462066bbee1c6e2a8e17cee69b
Michael Kovich <kovich@michaelkovich.com>
2018-05-04 11:14:51 -0500

    Added updateProperty, updated addUnit

--- db/addUnit.sql
+++ db/addUnit.sql
@@ -1,2 +1,2 @@
-insert into units (size, occupied, bed, bath, roomnum, propertyid) values ($1, $2, $3, $4, $5, $6)
-returning *;
+insert into units (size, occupied, bed, bath, roomnum, propertyid, rent) values ($1, $2, $3, $4, $5, $6, $7)
+SELECT * from units;
