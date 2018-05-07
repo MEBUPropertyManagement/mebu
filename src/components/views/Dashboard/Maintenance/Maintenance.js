4f7a10e9809e70ecb0e12b572af228886454f155
uukomadu <ugoukomadu@gmail.com>
2018-05-07 10:40:14 -0500

    archiving updates and resident login

--- src/components/views/Dashboard/Maintenance/Maintenance.js
+++ src/components/views/Dashboard/Maintenance/Maintenance.js
@@ -1,40 +1,35 @@
 import React, {Component} from 'react';
 import {getWorkorderById, closeWorkorderById} from '../../../../redux/ducks/workorderReducer';
 import {connect} from 'react-redux';
+import {isThisSecond} from 'date-fns';
 
 class Maintenance extends Component {
-  constructor(props) {
-    super(props);
-
-    // this.state = {
-    //   workorderid: '',
-    // };
-  }
-
   componentDidMount() {
     console.log(this.props);
     this.props.getWorkorderById(this.props.match.params.id);
-  }
-
-  closeWorkorderHandler() {
-    console.log(this.props);
-    // this.props.closeWorkorderHandler(this.props.match.params.id);
   }
 
   render() {
     const workorders =
       this.props.workorders &&
       this.props.workorders[0] &&
-      this.props.workorders.map(workorder => (
-        <div>
-          <div>Work order ID: {workorder.workorderid}</div>
-          <div>Resident First Name: {workorder.firstname}</div>
-          <div>Resident last Name: {workorder.lastname}</div>
-          <div>Unit ID: {workorder.unitid}</div>
-          <div>Work order ID: {workorder.workorderid}</div>
-          <div>Content: {workorder.content}</div>
-        </div>
-      ));
+      this.props.workorders.map((workorder) => {
+        const workorderid = workorder.workorderid;
+        console.log(typeof workorderid);
+        return (
+          <div>
+            <div>Work order ID: {workorder.workorderid}</div>
+            <div>Resident First Name: {workorder.firstname}</div>
+            <div>Resident last Name: {workorder.lastname}</div>
+            <div>Unit ID: {workorder.unitid}</div>
+            <div>Work order ID: {workorder.workorderid}</div>
+            <div>Content: {workorder.content}</div>
+            <button onClick={() => this.props.closeWorkorderById(workorderid)}>
+              Close this order
+            </button>
+          </div>
+        );
+      });
     return (
       <div>
         <div>{workorders}</div>
@@ -46,4 +41,4 @@ const mapStateToProps = state => ({
   ...state.workorderReducer,
 });
 
-export default connect(mapStateToProps, {getWorkorderById})(Maintenance);
+export default connect(mapStateToProps, {getWorkorderById, closeWorkorderById})(Maintenance);
