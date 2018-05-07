6e30d3e8d8d8e78e0483b9aff899264ffceb1a42
Michael Kovich <kovich@michaelkovich.com>
2018-05-07 16:53:23 -0500

    Updated authenticationController, workorderController, and PayBills

--- server/controllers/workorderControllers.js
+++ server/controllers/workorderControllers.js
@@ -18,17 +18,15 @@ const workOrderByResidentId = (req, res) => {
 
 const addWorkOrder = (req, res) => {
   const db = req.app.get('db');
-  const {
-    content, urgency, propertyid, unitid,
-  } = req.body;
+  const {content, urgency} = req.body;
   db
     .addWorkOrder([
       moment().format('MMMM Do YYYY'),
       content,
       urgency,
-      propertyid,
+      req.session.user.propertyid,
       req.session.user.userid,
-      unitid,
+      req.session.user.unitid,
     ])
     .then(response => res.status(200).json({added: true, response: response[0]}))
     .catch(err => res.status(200).json({error: `${err}`}));
