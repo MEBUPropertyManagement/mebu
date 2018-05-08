99a5010c39dd2d5395bd9123155fc3ea55226ebe
Eyobell Berhe <eyobell.berhe@live.longwood.edu>
2018-05-08 09:53:07 -0500

    message

--- src/components/views/Dashboard/ResidentDashboard/Maintenance/CreateWorkorder.js
+++ src/components/views/Dashboard/ResidentDashboard/Maintenance/CreateWorkorder.js
@@ -1,24 +1,45 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {loginResident} from '../../../../../redux/ducks/userReducer';
 
 class CreateWorkorder extends Component {
   constructor() {
     super();
 
     this.state = {
-      dateStart: '',
       content: '',
       urgency: '',
     };
+
+    handleSubmit = () => {};
   }
   render() {
     return (
       <div>
-        <h1>
-        CreateWorkorder
-        </h1>
-        <form>
-          <label> Comment </label>
-          <textarea />
+        <h1>CreateWorkorder</h1>
+        <form onSubmit={() => this.handleSubmit}>
+          <div>
+            <label> Content </label>
+            <textarea />
+          </div>
+          <br />
+          <div className="radio">
+            <label>Urgency</label>
+            <label>
+              <input type="radio" value="option1" checked />
+              Option 1
+            </label>
+          </div>
+          <div className="radio">
+            <label>
+              <input type="radio" value="option2" />
+              Option 2
+            </label>
+          </div>
+          <div className="radio">
+            <input type="radio" value="option3"> Option 3 </input>
+          </div>
         </form>
       </div>
     );
