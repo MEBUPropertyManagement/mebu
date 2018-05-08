ef1769352d2ac1b737c35bb67b683db63f7e8132
uukomadu <ugoukomadu@gmail.com>
2018-05-07 20:35:43 -0500

    working on history

--- src/components/views/Dashboard/ResidentDashboard/Maintenance/WorkorderHistory.js
+++ src/components/views/Dashboard/ResidentDashboard/Maintenance/WorkorderHistory.js
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {getWorkorderById} from '../../../../../redux/ducks/workorderReducer';
+import {connect} from 'react-redux';
 
-const WorkorderHistory = () => <div>WorkorderHistory</div>;
+class WorkorderHistory extends Component {
+  componentDidMount() {
+    console.log(this.props);
+    this.props.getWorkorderById(this.props.match.params.id);
+  }
 
-export default WorkorderHistory;
+  render() {
+    const workorders =
+      this.props.workorders &&
+      this.props.workorders[0] &&
+      this.props.workorders.map(workorder => (
+        <div>
+          <div>Work order ID: {workorder.workorderid}</div>
+          <div>Date Start: {workorder.datestart}</div>
+          <div>Date End: {workorder.dateend}</div>
+          <div>Resident First Name: {workorder.firstname}</div>
+          <div>Resident last Name: {workorder.lastname}</div>
+          <div>Unit ID: {workorder.unitid}</div>
+          <div>Work order ID: {workorder.workorderid}</div>
+          <div>Content: {workorder.content}</div>
+        </div>
+      ));
+    return <div>{workorders}</div>;
+  }
+}
+
+const mapStateToProps = state => ({
+  ...state.workorderReducer,
+});
+
+export default connect(mapStateToProps, {getWorkorderById})(WorkorderHistory);
