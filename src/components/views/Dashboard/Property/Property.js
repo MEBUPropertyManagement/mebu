66719edb863182593049ce493a7ca30c04b7bc5a
uukomadu <ugoukomadu@gmail.com>
2018-05-08 18:44:49 -0500

    new designs

--- src/components/views/Dashboard/Property/Property.js
+++ src/components/views/Dashboard/Property/Property.js
@@ -6,23 +6,41 @@ import './Property.css';
 
 class Property extends Component {
   componentDidMount() {
-    console.log(this.props);
     this.props.getPropertyById(this.props.match.params.id);
   }
 
   render() {
     let property = <p>...loading</p>;
     if (this.props.selectedProperty && !this.props.loading) {
-      console.log(this.props.selectedProperty);
       const prop = this.props.selectedProperty;
       property = (
-        <div>
-          <h2 className="Property-name">{prop.name}</h2>
-          <p>Address: {prop.address}</p>
-          <p>Units: {prop.units}</p>
-          <p>Value: {prop.value}</p>
-          <p>Expenses: {prop.expenses}</p>
-          <img className="Property__photo" src={prop.photourl} alt="apartment" />
+        <div className="Property">
+          <div className="Property__text">
+            <h5 className="Property-name">{prop.name}</h5>
+            <div className="Property__border__bottom" />
+            <p className="Property__address">{prop.address}</p>
+
+            <div className="Property__info">
+              <div className="Property__info__section">
+                <h6 className="Property__info__header">Units</h6>
+                <p className="Proprty__info__number">{prop.units}</p>
+              </div>
+
+              <div className="Property__info__section">
+                <h6 className="Property__info__header">Value</h6>
+                <p className="Proprty__info__number">${prop.value}</p>
+              </div>
+
+              <div className="Property__info__section">
+                <h6 className="Property__info__header">Expenses</h6>
+                <p className="Proprty__info__number">${prop.expenses}</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="Property__photo-container">
+            <img className="Property__photo" src={prop.photourl} alt="apartment" />
+          </div>
         </div>
       );
     }
