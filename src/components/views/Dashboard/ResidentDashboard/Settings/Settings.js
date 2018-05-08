1bf25f0c127c7f4ddf279094e32c6bc86c04c27a
bkchu <brandon.k.chung@gmail.com>
2018-05-08 10:37:12 -0500

    working on resident settings

--- src/components/views/Dashboard/ResidentDashboard/Settings/Settings.js
+++ src/components/views/Dashboard/ResidentDashboard/Settings/Settings.js
@@ -1,11 +1,65 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
-const Settings = () => {
-  return (
-    <div>
-      <p>This is the Resident Settings.</p>
-    </div>
-  );
-};
+class Settings extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      firstname: '',
+      lastname: '',
+    };
 
-export default Settings;
+    this.onSubmitHandler = this.onSubmitHandler.bind(this);
+  }
+
+  componentDidMount() {
+    console.log(this.props.current_user);
+  }
+
+  onSubmitHandler(e) {
+    e.preventDefault();
+  }
+
+  onChangeHandler(e) {
+    const {email, firstname, lastname} = this.state;
+    if (email && firstname && lastname) {
+      this.setState({[e.target.name]: e.target.value});
+    }
+  }
+
+  render() {
+    const {email, firstname, lastname} = this.state;
+    return (
+      <div className="Settings">
+        <form onSubmit={this.onSubmitHandler}>
+          <input
+            onChange={this.onChangeHandler}
+            className="Settings__input Settings__input--email"
+            value={email}
+            name="email"
+            type="email"
+          />
+          <input
+            onChange={this.onChangeHandler}
+            className="Settings__input Settings__input--firstname"
+            value={firstname}
+            name="firstname"
+            type="text"
+          />
+          <input
+            onChange={this.onChangeHandler}
+            className="Settings__input Settings__input--lastname"
+            value={lastname}
+            name="lastname"
+            type="text"
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({...state.userReducer});
+
+export default connect(mapStateToProps, null)(Settings);
