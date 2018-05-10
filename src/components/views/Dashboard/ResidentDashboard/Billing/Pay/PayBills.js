8c5cef1d6343a9f3e86af36e9afbf7e73cef721d
mrisby <35502928+mrisby@users.noreply.github.com>
2018-05-10 09:14:32 -0500

    pull request

--- src/components/views/Dashboard/ResidentDashboard/Billing/Pay/PayBills.js
+++ src/components/views/Dashboard/ResidentDashboard/Billing/Pay/PayBills.js
@@ -1,8 +1,10 @@
-import React, {Component} from 'react';
-import StripeCheckout from 'react-stripe-checkout';
-import axios from 'axios';
-import {connect} from 'react-redux';
-import {getBills} from '../../../../../../redux/ducks/residentReducer';
+import React, { Component } from "react";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+import { connect } from "react-redux";
+import { getBills } from "../../../../../../redux/ducks/residentReducer";
+
+import "./PayBills.css";
 
 class PayBills extends Component {
   constructor() {
@@ -10,7 +12,7 @@ class PayBills extends Component {
 
     this.state = {
       amount: 0,
-      billid: 0,
+      billid: 0
     };
   }
 
@@ -22,22 +24,22 @@ class PayBills extends Component {
     console.log(token);
 
     axios
-      .post('/bills/pay', {
+      .post("/bills/pay", {
         amount: this.state.amount,
         stripeToken: token,
-        billid: this.state.billid,
+        billid: this.state.billid
       })
       .then(res => console.log(res)) // call getBills
       .catch(err => console.log(err));
   }
 
   selectBill(value) {
-    const {bills} = this.props.bills;
+    const { bills } = this.props.bills;
     const bucketOfMonkeys = bills.findIndex(element => element.amount == value);
 
     this.setState({
       amount: +bills[bucketOfMonkeys].amount,
-      billid: +bills[bucketOfMonkeys].billid,
+      billid: +bills[bucketOfMonkeys].billid
     });
   }
 
@@ -45,9 +47,9 @@ class PayBills extends Component {
     let mappedItems = [];
 
     const re = /\b(\d+)(\d{2})\b/;
-    const subst = '$1.$2';
+    const subst = "$1.$2";
 
-    const {bills} = this.props.bills;
+    const { bills } = this.props.bills;
 
     if (bills && bills[0]) {
       mappedItems = bills.map(bill => (
@@ -79,7 +81,7 @@ class PayBills extends Component {
               </StripeCheckout>
             </form>
           ) : (
-            'No bills to pay, good sir. *tips hat*'
+            "No bills to pay, good sir. *tips hat*"
           )}
         </div>
       </div>
@@ -88,7 +90,7 @@ class PayBills extends Component {
 }
 
 const mapStateToProps = state => ({
-  ...state.residentReducer,
+  ...state.residentReducer
 });
 
-export default connect(mapStateToProps, {getBills})(PayBills);
+export default connect(mapStateToProps, { getBills })(PayBills);
