66868cdac60c2fa010f6bf4b9aae4a0766154c45
Michael Kovich <kovich@michaelkovich.com>
2018-05-17 21:51:42 -0500

    Sorted workorders by workorderid.

--- db/workorders_getByProperty.sql
+++ db/workorders_getByProperty.sql
@@ -1,3 +1,4 @@
 SELECT workorders.workorderid, workorders.datestart, workorders.dateend, workorders.content, workorders.urgency, workorders.propertyid, workorders.residentid, workorders.unitid, residents.firstname, residents.lastname FROM workorders
 INNER JOIN residents ON residents.residentid = workorders.residentid
-WHERE workorders.propertyid = $1;
+WHERE workorders.propertyid = $1
+ORDER BY workorders.workorderid;
